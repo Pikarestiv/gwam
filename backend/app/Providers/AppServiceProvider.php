7acd897c80095f8799c05e3198a7abd27c9d8ea5
pikarestiv <okwyodumodu@gmail.com>
2026-02-19 14:47:56 +0100

    fix(backend): set default string length to 191 for MySQL shared hosting

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +12,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+    //
     }
 
     /**
@@ -19,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Fix for MySQL < 5.7.7 / MariaDB < 10.2 on shared hosting
+        Schema::defaultStringLength(191);
     }
 }
